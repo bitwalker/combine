a6d2d023f2ca325558205da290d998aeec6bec7f
Paul Schoenfelder <paulschoenfelder@gmail.com>
2015-07-30 14:33:11 -0500

    Add test for mapping error case

--- test/combine_test.exs
+++ test/combine_test.exs
@@ -41,4 +41,9 @@ defmodule CombineTest do
     assert ["+0230", "-0400"] = Combine.parse("+02:30-0400", parser)
     assert ["-0230", "+0400"] = Combine.parse("-02:30+0400", parser)
   end
+
+  test "can map with fail to produce customized errors" do
+    parser = map(digit, fn x when div(x, 2) == 0 -> x; y -> {:error, "#{y} is not an even number!"} end) |> digit
+    assert {:error, "5 is not an even number!"} = Combine.parse("50", parser)
+  end
 end
