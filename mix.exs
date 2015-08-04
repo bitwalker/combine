d5ef3a191bb22afd0d027ba745bfea3c5ce276e6
Paul Schoenfelder <paulschoenfelder@gmail.com>
2015-08-04 15:25:56 -0500

    Version 0.4.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Combine.Mixfile do
 
   def project do
     [app: :combine,
-     version: "0.3.4",
+     version: "0.4.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
