42d7de9b71ba7b607a969b4264a52fe893108871
Sasa Juric <sasa.juric@gmail.com>
2016-06-08 21:19:34 +0200

    document defparser

--- lib/combine/parser_state.ex
+++ lib/combine/parser_state.ex
@@ -1,5 +1,16 @@
 defmodule Combine.ParserState do
-  @moduledoc false
+  @moduledoc """
+  Defines a struct representing the state of the parser.
+
+  The struct has following fields:
+
+    - `input` - the unparsed part of the input
+    - `column` - column position of the next character (zero based)
+    - `line` - current line position
+    - `results` - list of outputs produced by so far, in the reverse order
+    - `status` - `:ok` if the grammar rules are satisfied, `:error` otherwise
+    - `error` - an error message if a grammar rule wasn't satisfied
+  """
   defstruct input: <<>>,
             column: 0,
             line: 1,
